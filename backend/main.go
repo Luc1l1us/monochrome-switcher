0775daf355a72b0ac2d2ef93fdb30b53d565beae
Luc1l1us <ikazuha001@gmail.com>
2026-03-15 21:53:47 +0800

    Gemini func can now be accessed thru CLI, havent tried with Wails-frontend

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"backend/provider.go/Agents/Cloud"
+	"fmt"
+)
+
+func EnterPrompt() string {
+
+	var prompt string
+
+	fmt.Println("Please enter your prompt...")
+	fmt.Scanln(&prompt)
+	result := chatGPTProvider(prompt)
+	fmt.Println(result)
+	return result
+}
+
+func main() {
+	EnterPrompt()
+	userprompt := EnterPrompt()
+	Cloud.CallLLM(userprompt)
+}
--- backend/provider.go
+++ backend/provider.go
@@ -1,17 +1,5 @@
 package main
 
-import "fmt"
-
-func main() {
-
-	var prompt string
-
-	fmt.Println("Please enter your prompt...")
-	fmt.Scanln(&prompt)
-	result := chatGPTProvider(prompt)
-	fmt.Println(result)
-}
-
 func check_online() {
 
 }
