f5618dee507a6b9fa07cff25cbb5c8cd455d4696
Luc1l1us <ikazuha001@gmail.com>
2026-04-13 19:45:51 +0800

    imported properly radix, fixed README, fixed package.json prob

--- rails-frontend/frontend/vite.config.js
+++ rails-frontend/frontend/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['@radix-ui/react-slot', '@radix-ui/react-icons'],
+    },
+  },
   plugins: [react()]
 })
+
+
