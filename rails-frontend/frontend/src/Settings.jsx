b5d5af82e06aae0e650fc0eb126f4e096dc99bbc
Luc1l1us <ikazuha001@gmail.com>
2026-05-14 00:00:50 +0800

    moved agent functionality to aiselectionscreen

--- rails-frontend/frontend/src/Settings.jsx
+++ rails-frontend/frontend/src/Settings.jsx
@@ -3,8 +3,31 @@ import NavBar from './components/NavBar';
 
 export default function Settings() {
     return (
-        <div>
-            TESTING SETTINGS
+        <div id="settings">
+            <div id='Title'>
+                Settings
+            </div>
+            <div id='settings-content'>
+                <div className="content-title">
+                    <h2>
+                        What would you like to do today?
+                    </h2>
+                    <h3 className="smol">
+                        Select one of the cards below to continue.
+                    </h3>
+                </div>
+                <div id='content-cards'>
+                    <div className='card'>
+                        Start a new multi-agent chat
+                    </div>
+                    <div className='card'>
+                        Use a template
+                    </div>
+                    <div className='card'>
+                        Continue where I left off
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
