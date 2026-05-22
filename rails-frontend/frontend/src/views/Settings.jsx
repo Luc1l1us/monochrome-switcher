280bb827441a7126d1361ec52e23c97874904c25
Luc1l1us <ikazuha001@gmail.com>
2026-05-22 23:23:40 +0800

    made imageids unique

--- rails-frontend/frontend/src/views/Settings.jsx
+++ rails-frontend/frontend/src/views/Settings.jsx
@@ -21,7 +21,7 @@ export default function Settings() {
                     <div id="first-column">
                         <div className="agent">
                             <div className="agent-image">
-                                <img id="gemini-api-image" src={claudeIcon}></img>
+                                <img id="claude-image" src={claudeIcon}></img>
                             </div>
                             <div className="agent-text">
                                 <a href="https://platform.claude.com/settings/keys">
@@ -36,7 +36,7 @@ export default function Settings() {
                         </div>
                         <div className="agent">
                             <div className="agent-image">
-                                <img id="gemini-api-image" src={chatgptIcon}></img>
+                                <img id="chatgpt-image" src={chatgptIcon}></img>
                             </div>
                             <div className="agent-text">
                                 <a href="https://openai.com/api/">
@@ -51,7 +51,7 @@ export default function Settings() {
                         </div>
                         <div className="agent">
                             <div className="agent-image">
-                                <img id="gemini-api-image" src={geminiIcon}></img>
+                                <img id="gemini-image" src={geminiIcon}></img>
                             </div>
                             <div className="agent-text">
                                 <a href="https://ai.google.dev/gemini-api/docs">
@@ -71,7 +71,7 @@ export default function Settings() {
                     <div id="second-column">
                         <div className="agent">
                             <div className="agent-image">
-                                <img id="gemini-api-image" src={claudeIcon}></img>
+                                <img id="perplex-image" src={claudeIcon}></img>
                             </div>
                             <div className="agent-text">
                                 <a href="https://console.perplexity.ai/">
@@ -87,7 +87,7 @@ export default function Settings() {
 
                         <div className="agent">
                             <div className="agent-image">
-                                <img id="gemini-api-image" src={claudeIcon}></img>
+                                <img id="deepseek-image" src={claudeIcon}></img>
                             </div>
                             <div className="agent-text">
                                 <a href="https://platform.deepseek.com/api_keys">
