02f45cbb1f935173f9882bc8f1871319d56f19a7
Luc1l1us <ikazuha001@gmail.com>
2026-07-16 22:55:45 +0800

    unfinished revamp of MSC

--- rails-frontend/frontend/src/components/MSC.jsx
+++ rails-frontend/frontend/src/components/MSC.jsx
@@ -2,29 +2,24 @@ import {useState} from 'react';
 import * as icons from "../../../../icons"
 
 function MessengerContainer({submittedPrompt, resultText2, convo, selected}) {
-    
+    const [conversation, setConversation] = useState([]);    
     return (
     <div id='Messenger-container'>
-        {/* Show this when the convo variable is empty */}
         { !convo && (
             <div id="prompt" className="prompt">Please enter your prompt: </div>
         )}
-        {/* we might need to make this a separate component */}
         <div id="OutputBox">
-            {/* HIDE THE USER PROMPT WHEN THERE IS NO CONVO */}
             { convo && (
                 <div id="user-prompt-container">
                     <div className='text'>
                         {submittedPrompt}
                     </div>
                     <div className='avatar'>
-                        {/* THIS IS PLACEHOLDER FOR NOW */}
                         <img src={icons.userdefaultIcon}></img>
                     </div>
                 </div>
                 )
             }
-            {/* HIDE THE AI PROMPT WHEN THERE IS NO CONVO */}
             { convo && (
                 <div id="ai-response">
                     <div className='avatar'>
