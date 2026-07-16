0ed13d72947b5974d56b8e4bf32e7d068e51ccb7
Luc1l1us <ikazuha001@gmail.com>
2026-07-16 23:02:54 +0800

    created a new component for messagebubble

---
+++ rails-frontend/frontend/src/components/MessageBubble.jsx
@@ -0,0 +1,13 @@
+function MessageBubble(avatar) {
+    const [conversation, setConversation] = useState([]); 
+
+// Make the avatar alternate between 
+<div id="user-prompt-container">
+    <div className='text'>
+        {submittedPrompt}
+    </div>
+    <div className='avatar'>
+        <img src={icons.userdefaultIcon}></img>
+    </div>
+</div>
+}
