189d62cc0e66e72372a012cc12651747498d1220
Luc1l1us <ikazuha001@gmail.com>
2026-08-22 23:02:56 +0800

    fixed position of delete button

--- rails-frontend/frontend/src/components/HistoryCards.jsx
+++ rails-frontend/frontend/src/components/HistoryCards.jsx
@@ -1,13 +1,20 @@
+import * as icons from "../../../../icons"
+
 export default function HistoryCards({ chat, onClick }) {
     return (
-        <div className="card" onClick={() => {
+        <div className="card" id="historycard-id" onClick={() => {
             console.log("Clicked chat:", chat.id, "Chat Title:",chat.title, chat.provider)
             onClick(chat.id)
             }}>
             <h3 className="history-smol">{chat.title}</h3>
             <p className="history-smol">{chat.provider}</p>
             <small className="history-smol">{chat.created_at}</small>
-            <small>{chat.id}</small>
+            <small className="history-smol">{chat.id}</small>
+            <small className="delete-button">
+                <button className="delete-button">
+                    <img id="nav-settings" src={icons.trashicon}></img>
+                </button>
+            </small>
         </div>
     )
 }
